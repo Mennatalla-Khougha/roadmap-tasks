1f41e61d9f0cf77a0937d8cb2700a164631a9060
Mennatalla-Khougha <menmen.khougha@gmail.com>
2025-05-18 16:53:39 +0300

    Add user creation and response models

--- models/user_model.py
+++ models/user_model.py
@@ -1,9 +1,23 @@
 from pydantic import BaseModel, EmailStr
 
 
-class User(BaseModel):
+class UserCreate(BaseModel):
     id: str
     username: str
     email: EmailStr
     password: str
     is_active: bool = False
+
+
+class UserResponse(BaseModel):
+    id: str
+    username: str
+    email: EmailStr
+    is_active: bool = False
+
+    class Config:
+        from_attributes = True
+
+
+
+
