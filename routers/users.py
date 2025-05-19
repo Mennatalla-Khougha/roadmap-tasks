425a32bd6b28c78eec7a1a3abccd77acc9981b7c
Mennatalla-Khougha <menmen.khougha@gmail.com>
2025-05-19 12:48:27 +0300

    Add user retrieval functionality and update user creation logic

--- routers/users.py
+++ routers/users.py
@@ -1,17 +1,35 @@
 from fastapi import  APIRouter, HTTPException
+from pydantic import EmailStr
+
 from models.user_model import  UserCreate, UserResponse
-from services.user_services import create_user
+from services.user_services import create_user, get_user
 
 router = APIRouter()
 
 @router.post("/register", response_model=UserResponse)
-async def create_user_endpoint(user: UserCreate):
+def create_user_endpoint(user: UserCreate):
     """
     Endpoint to create a new user.
     """
     try:
-        return await create_user(user)
+        return create_user(user)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected Error: {str(e)}")
+
+
+@router.get("/users/{email}", response_model=UserResponse)
+def get_user_endpoint(email: EmailStr):
+    """
+    Endpoint to get a user by email.
+    """
+    try:
+        user = get_user(email)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        return user
+    except FileNotFoundError as e:
+        raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Unexpected Error: {str(e)}")
--- services/user_services.py
+++ services/user_services.py
@@ -1,24 +1,44 @@
+from pydantic import EmailStr
+
 from core.security import hash_password
-from models.user_model import UserCreate
+from models.user_model import UserCreate, UserResponse
 from core.database import db
 
-def create_user(user: UserCreate):
+def create_user(user: UserCreate) -> UserCreate:
     """
     Create a new user in the database.
     """
     try:
-        user_ref = db.collection("users").where ("email", "==", user.email).get()
-        if user_ref:
+        user_ref = db.collection("users").document(user.id).get()
+        if user_ref.exists:
             raise ValueError("Email already exists")
         hashed_password = hash_password(user.password)
         user_data = {
-            "id": user.id,
+            "id": user.email,
             "username": user.username,
             "email": user.email,
             "password": hashed_password,
             "is_active": user.is_active
         }
-        db.collection("users").add(user_data)
+        db.collection("users").document(user.id).set(user_data)
         return user
     except Exception as e:
         raise ValueError(f"Error creating user: {e}")
+
+
+def get_user(email: EmailStr):
+    """
+    Get a user by email from the database.
+    """
+    try:
+        if not email:
+            raise ValueError("Email is required")
+        user_ref = db.collection("users").document(email).get()
+        if not user_ref.exists:
+            raise FileNotFoundError("No user exist with that Email")
+        user_data = user_ref.to_dict()
+        return user_data
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"User not found: {e}")
+    except Exception as e:
+        raise ValueError(f"Error retrieving user: {e}")
