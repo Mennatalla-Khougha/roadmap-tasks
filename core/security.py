2d5fc9a5f415736b24f27fa8f0a63d748cf7abbc
Mennatalla-Khougha <menmen.khougha@gmail.com>
2025-06-20 23:05:19 +0300

    Enhance security module to support user roles and admin verification

--- core/security.py
+++ core/security.py
@@ -2,12 +2,13 @@ import os
 from typing import Annotated
 
 from dotenv import load_dotenv
-from fastapi import Depends, HTTPException
-from fastapi.security import HTTPBearer
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer
 from passlib.context import CryptContext
 from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
 from jose.exceptions import ExpiredSignatureError
+from schemas.user_model import TokenData, UserRole
 
 load_dotenv()
 
@@ -16,7 +17,7 @@ ALGORITHM = os.getenv("JWT_ALGORITHM")
 
 # oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 oauth2_scheme = HTTPBearer()
-
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
 # Create a password context with the default hashing ALGORITHM
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -48,12 +49,14 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 def create_access_token(
     subject: str,
     user_id: str,
+    user_role: str = "user"
 ) -> str:
     """
     Create a JWT access token with a subject and user ID.
     Args:
         subject (str): The subject of the token, typically the user's email.
         user_id (str): The unique identifier for the user.
+        user_role (str): The role of the user, default is "user".
     Returns:
         str: The encoded JWT token.
     """
@@ -66,13 +69,14 @@ def create_access_token(
         "sub": subject,
         "id": user_id,
         "iat": now,
-        "exp": expire
+        "exp": expire,
+        "role": user_role
     }
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> str:
+def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> TokenData:
     """
     Get the current user from the JWT token.
     Args:
@@ -89,8 +93,30 @@ def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> str:
         if user_id is None:
             raise HTTPException(
                 status_code=401, detail="Invalid token payload")
-        return user_id
+        return TokenData(
+            email=payload.get("sub"),
+            user_id=user_id,
+            role=payload.get("role")
+        )
     except ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Session Expired")
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
+
+
+async def get_current_admin_user(current_user: TokenData = Depends(get_current_user)) -> TokenData:
+    """
+    Dependency to ensure the current user is an admin.
+    Args:
+        current_user (TokenData): The current user data obtained from the JWT token.
+    Raises:
+        HTTPException: If the user is not an admin.
+    Returns:
+        TokenData: The current user data if the user is an admin.
+    """
+    if current_user.role != UserRole.ADMIN:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have permission to perform this action",
+        )
+    return current_user
