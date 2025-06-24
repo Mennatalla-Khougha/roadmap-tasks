266acdac914a715b12af0b59a303b56afec7c009
Mennatalla-Khougha <menmen.khougha@gmail.com>
2025-06-24 23:13:35 +0300

    Integrate Firebase Admin SDK for Firestore access and update requirements

--- core/database.py
+++ core/database.py
@@ -1,7 +1,9 @@
+import firebase_admin
 import redis
 from google.cloud import firestore
 import os
 from dotenv import load_dotenv
+from firebase_admin import credentials, firestore
 
 load_dotenv()
 
@@ -9,11 +11,28 @@ load_dotenv()
 db = None
 r = None
 
+firebase_credentials = {
+  "type": os.getenv("FIREBASE_TYPE"),
+  "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+  "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+  "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\n', '\n'),
+  "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+  "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+  "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+  "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+  "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+  "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+}
+
+cred = credentials.Certificate(firebase_credentials)
+firebase_admin.initialize_app(cred)
+
 
 def get_db():
     global db
     if db is None:
-        db = firestore.Client()
+        db = firestore.client()
     return db
 
 
